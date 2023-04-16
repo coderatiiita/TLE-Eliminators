929b1d5f9caa8fe7f9a24b88f32575e732057043
Shubham Vashishtha <shubham@onehouse.ai>
2023-04-16 22:05:50 +0530

    day2 removing digits and CP template added

---
+++ src/CPTemplate.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class CPTemplate {
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+        // Read input and solve the problem
+        // Example: Read an integer and print it
+        int n = in.nextInt();
+        out.println(n);
+
+        // Flush and close the output PrintWriter
+        out.flush();
+        out.close();
+    }
+
+    // Fast I/O class for reading input
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
