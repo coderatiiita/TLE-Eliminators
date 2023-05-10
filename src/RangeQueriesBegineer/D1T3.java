1af43a3bedd07625a349c37d8fd1ddf05569e52b
Shubham Vashishtha <16306985+shubhvash@users.noreply.github.com>
2023-05-11 00:57:11 +0530

    Range Query problem added

--- src/RangeQueriesBegineer/D1T3.java
+++ src/RangeQueriesBegineer/D1T3.java
@@ -3,8 +3,8 @@ package RangeQueriesBegineer;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class D1T2 {
-  public static void main(String[] args) throws IOException {
+public class D1T3 {
+  public static void main(String[] args) {
     FastReader in = new FastReader();
     PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
@@ -27,7 +27,8 @@ public class D1T2 {
       } else {
         int l = in.nextInt();
         int r = in.nextInt();
-        out.println(st.query(l, r - 1));
+        Pair ans = st.query(l, r - 1);
+        out.printf("%d %d\n", ans.min, ans.count);
       }
     }
 
@@ -80,41 +81,37 @@ public class D1T2 {
   }
 
   static class SegmentTree {
-    private long[] tree;
+    private Pair[] tree;
     private long[] arr;
     private int n;
 
     public SegmentTree(long[] arr) {
       this.arr = arr;
       this.n = arr.length;
-      this.tree = new long[4 * n + 4];
+      this.tree = new Pair[4 * n];
       build(1, 0, n - 1);
     }
 
     private void build(int v, int tl, int tr) {
       if (tl == tr) {
-        tree[v] = arr[tl];
+        tree[v] = new Pair(arr[tl], 1);
       } else {
         int tm = (tl + tr) / 2;
         build(2 * v, tl, tm);
         build(2 * v + 1, tm + 1, tr);
-        tree[v] = Math.min(tree[2 * v], tree[2 * v + 1]);
+        tree[v] = merge(tree[2 * v], tree[2 * v + 1]);
       }
     }
 
-    public long query(int l, int r) {
+    public Pair query(int l, int r) {
       return query(1, 0, n - 1, l, r);
     }
 
-    private long query(int v, int tl, int tr, int l, int r) {
-      if (l > r) {
-        return Integer.MAX_VALUE;
-      }
-      if (l == tl && r == tr) {
-        return tree[v];
-      }
+    private Pair query(int v, int tl, int tr, int l, int r) {
+      if (l > r) return new Pair(Integer.MAX_VALUE, 0);
+      if (l == tl && r == tr) return tree[v];
       int tm = (tl + tr) / 2;
-      return Math.min(
+      return merge(
           query(2 * v, tl, tm, l, Math.min(r, tm)),
           query(2 * v + 1, tm + 1, tr, Math.max(l, tm + 1), r));
     }
@@ -125,16 +122,29 @@ public class D1T2 {
 
     private void update(int v, int tl, int tr, int pos, long newVal) {
       if (tl == tr) {
-        tree[v] = newVal;
+        tree[v] = new Pair(newVal, 1);
       } else {
         int tm = (tl + tr) / 2;
-        if (pos <= tm) {
-          update(2 * v, tl, tm, pos, newVal);
-        } else {
-          update(2 * v + 1, tm + 1, tr, pos, newVal);
-        }
-        tree[v] = Math.min(tree[2 * v], tree[2 * v + 1]);
+        if (pos <= tm) update(2 * v, tl, tm, pos, newVal);
+        else update(2 * v + 1, tm + 1, tr, pos, newVal);
+        tree[v] = merge(tree[2 * v], tree[2 * v + 1]);
       }
+    }
+
+    private Pair merge(Pair a, Pair b) {
+      if (a.min < b.min) return new Pair(a.min, a.count);
+      if (a.min > b.min) return new Pair(b.min, b.count);
+      return new Pair(a.min, a.count + b.count);
+    }
+  }
+
+  static class Pair {
+    long min;
+    long count;
+
+    Pair(long min, long count) {
+      this.min = min;
+      this.count = count;
     }
   }
 }
