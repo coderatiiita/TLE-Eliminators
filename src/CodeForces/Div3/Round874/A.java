3ebd6e969614cbe43078e3e96633b63fc22200a5
Shubham Vashishtha <shubham@onehouse.ai>
2023-05-21 14:42:44 +0530

    874 div 3 | A B added

--- src/CodeForces/Div3/Round874/A.java
+++ src/CodeForces/Div3/Round874/A.java
@@ -7,142 +7,119 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class A {
-    static class Reader {
-        final private int BUFFER_SIZE = 1 << 16;
-        private final DataInputStream din;
-        private final byte[] buffer;
-        private int bufferPointer, bytesRead;
+  static class Reader {
+    private final int BUFFER_SIZE = 1 << 16;
+    private final DataInputStream din;
+    private final byte[] buffer;
+    private int bufferPointer;
+    private int bytesRead;
 
-        public Reader()
-        {
-            din = new DataInputStream(System.in);
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public Reader(String file_name) throws IOException
-        {
-            din = new DataInputStream(
-                    new FileInputStream(file_name));
-            buffer = new byte[BUFFER_SIZE];
-            bufferPointer = bytesRead = 0;
-        }
-
-        public String readLine() throws IOException
-        {
-            byte[] buf = new byte[64]; // line length
-            int cnt = 0, c;
-            while ((c = read()) != -1) {
-                if (c == '\n') {
-                    if (cnt != 0) {
-                        break;
-                    }
-                    else {
-                        continue;
-                    }
-                }
-                buf[cnt++] = (byte)c;
-            }
-            return new String(buf, 0, cnt);
-        }
-
-        public int nextInt() throws IOException
-        {
-            int ret = 0;
-            byte c = read();
-            while (c <= ' ') {
-                c = read();
-            }
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public long nextLong() throws IOException
-        {
-            long ret = 0;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        public double nextDouble() throws IOException
-        {
-            double ret = 0, div = 1;
-            byte c = read();
-            while (c <= ' ')
-                c = read();
-            boolean neg = (c == '-');
-            if (neg)
-                c = read();
-
-            do {
-                ret = ret * 10 + c - '0';
-            } while ((c = read()) >= '0' && c <= '9');
-
-            if (c == '.') {
-                while ((c = read()) >= '0' && c <= '9') {
-                    ret += (c - '0') / (div *= 10);
-                }
-            }
-
-            if (neg)
-                return -ret;
-            return ret;
-        }
-
-        private void fillBuffer() throws IOException
-        {
-            bytesRead = din.read(buffer, bufferPointer = 0,
-                    BUFFER_SIZE);
-            if (bytesRead == -1)
-                buffer[0] = -1;
-        }
-
-        private byte read() throws IOException
-        {
-            if (bufferPointer == bytesRead)
-                fillBuffer();
-            return buffer[bufferPointer++];
-        }
-
-        public void close() throws IOException
-        {
-            if (din == null)
-                return;
-            din.close();
-        }
+    public Reader() {
+      din = new DataInputStream(System.in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
     }
 
-    public static void main(String[] args) throws IOException {
-        Reader s = new Reader();
-        int t = s.nextInt();
-        while (t-- > 0) {
-            int n = s.nextInt();
-            String str = s.readLine();
-            Set<String> set = new HashSet<>();
-            for (int i=0; i<n-1; i++) {
-                String temp = str.substring(i, i+2);
-                set.add(temp);
-
-            }
-            System.out.printf("%d" + (t != 0 ? "\n" : ""), set.size());
-        }
+    public Reader(String file_name) throws IOException {
+      din = new DataInputStream(new FileInputStream(file_name));
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
     }
+
+    public String readLine() throws IOException {
+      byte[] buf = new byte[64]; // line length
+      int cnt = 0, c;
+      while ((c = read()) != -1) {
+        if (c == '\n') {
+          if (cnt != 0) {
+            break;
+          } else {
+            continue;
+          }
+        }
+        buf[cnt++] = (byte) c;
+      }
+      return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') {
+        c = read();
+      }
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public long nextLong() throws IOException {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public double nextDouble() throws IOException {
+      double ret = 0, div = 1;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (c == '.') {
+        while ((c = read()) >= '0' && c <= '9') {
+          ret += (c - '0') / (div *= 10);
+        }
+      }
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+    }
+
+    private byte read() throws IOException {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+      if (din == null) return;
+      din.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    Reader s = new Reader();
+    int t = s.nextInt();
+    while (t-- > 0) {
+      int n = s.nextInt();
+      String str = s.readLine();
+      Set<String> set = new HashSet<>();
+      for (int i = 0; i < n - 1; i++) {
+        String temp = str.substring(i, i + 2);
+        set.add(temp);
+      }
+      System.out.printf("%d" + (t != 0 ? "\n" : ""), set.size());
+    }
+  }
 }
